a5fb3729c0a92972aaa65002e836b813c59e8465
Baptiste CONTRERAS <38988658+BaptisteContreras@users.noreply.github.com>
2024-09-01 16:32:35 +0200

    feat: init

---
+++ README.md
@@ -0,0 +1,19 @@
+# EZ DELIVER
+
+Tool to create a release branch with commits from pull requests linked to a Github issue with specific labels
+
+Requires [Castor](https://castor.jolicode.com/) and PHP to work
+
+## Init
+
+```
+castor init
+```
+
+Then fill the config fields
+
+## Create a delivery
+
+```
+castor create-package <project>
+```
---
+++ castor.php
@@ -0,0 +1,593 @@
+<?php
+
+use Castor\Attribute\AsContext;
+use Castor\Attribute\AsListener;
+use Castor\Attribute\AsTask;
+use Castor\Context;
+use function Castor\capture;
+use function Castor\fs;
+use function Castor\http_request;
+use function Castor\io;
+use function Castor\load_dot_env;
+use function Castor\run;
+use function Castor\context;
+use function Castor\variable;
+
+
+const YES = 'yes';
+const NO = 'no';
+
+const DEFAULT_CONFIG_PATH = '~/.ez-delivery';
+const CONFIG_PATH_ENV_VAR = 'EZ_DELIVERY_CONFIG_PATH';
+#[AsTask(description: 'init project config')]
+function initProjectConfig(): void
+{
+    $context = context();
+    $configPath = $context->environment[CONFIG_PATH_ENV_VAR];
+
+    if (!fs()->exists($configPath)) {
+        fs()->mkdir($configPath);
+        io()->success(sprintf('%s config dir created', $configPath));
+    }
+
+    $config = [
+        'project' => io()->ask('Project name', validator: function ($value) use ($configPath) {
+            if (empty($value)) {
+                throw new \Exception('Project name cannot be empty');
+            }
+
+            if (fs()->exists(getProjectConfigPath($value, $configPath))) {
+                throw new \Exception(sprintf('project %s config already exists in %s', $value, $configPath));
+            }
+
+            return $value;
+        }),
+        'src' => io()->ask('Project source path', validator: function ($value) {
+            if (empty($value)) {
+                throw new \Exception('project source path name cannot be empty');
+            }
+
+            if (!fs()->exists($value)) {
+                throw new \Exception(sprintf('project %s does not exists', $value));
+            }
+
+            return $value;
+        }),
+        'baseBranch' => io()->ask('Base branch to create a package', default: 'develop', validator: function ($value) {
+            if (empty($value)) {
+                throw new \Exception('base branch cannot be empty');
+            }
+
+            return $value;
+        }),
+
+        'repo' => [
+            'owner' => io()->ask('Repository owner', validator: function ($value) {
+                if (empty($value)) {
+                    throw new \Exception('repository owner cannot be empty');
+                }
+
+                return $value;
+            }),
+            'name' => io()->ask('Repository name', validator: function ($value) {
+                if (empty($value)) {
+                    throw new \Exception('repository name cannot be empty');
+                }
+
+                return $value;
+            }),
+        ],
+        'githubToken' => io()->askHidden('Github personal token', validator: function ($value) {
+            if (empty($value)) {
+                throw new \Exception('Github personal token cannot be empty');
+            }
+
+            return $value;
+        }),
+        'envs' => [],
+    ];
+
+
+    $config['tmpStorage'] = io()
+        ->ask('Temporary storage directory for paused release', default: sprintf('%s/%s', $configPath, $config['project']), validator: function ($value) {
+            if (empty($value)) {
+                throw new \Exception('temporary storage directory cannot be empty');
+            }
+
+            if (!fs()->exists($value)) {
+                fs()->mkdir($value);
+            };
+
+            return sprintf('%s/%s', $value, 'last');
+        });
+
+    do {
+        io()->title('Add new environnement');
+        $newEnv = [
+            'name' => io()->ask('env name', validator: function ($value) use ($config) {
+                if (empty($value)) {
+                    throw new \Exception('env name cannot be empty');
+                }
+
+                if (count(array_filter($config['envs'], fn(array $env) => $env['name'] === $value)) > 0) {
+                    throw new \Exception(sprintf('env %s already exists', $value));
+                }
+
+                return $value;
+            }),
+            'alreadyDeliveredLabel' => io()->ask('"Already delivered" label name', validator: function ($value) {
+                if (empty($value)) {
+                    throw new \Exception('label name cannot be empty');
+                }
+
+                return $value;
+            }),
+            'toDeliverLabel' => io()->ask('"To deliver" label name', validator: function ($value) {
+                if (empty($value)) {
+                    throw new \Exception('label name cannot be empty');
+                }
+
+                return $value;
+            }),
+        ];
+
+        $config['envs'][] = $newEnv;
+
+        io()->success(sprintf('Successfully added new env %s', $newEnv['name']));
+    } while (YES === io()->choice('Add another env ?', [YES, NO], YES));
+
+
+    $projectConfigPath = getProjectConfigPath($config['project'], $configPath);
+    fs()->dumpFile($projectConfigPath, json_encode($config));
+
+    io()->success(sprintf('new project config %s stored at %s', $config['project'], $projectConfigPath));
+}
+
+function getProjectConfigPath(string $projectName, string $configPath): string
+{
+    return sprintf('%s/%s.json', $configPath, $projectName);
+}
+
+#[AsTask(description: 'Create a package')]
+function createPackage(string $project): void
+{
+    $configPath = context()->environment[CONFIG_PATH_ENV_VAR];
+    $config = json_decode(file_get_contents(getProjectConfigPath($project, $configPath)), true);
+
+    $envsName = array_map(fn(array $envData) => $envData['name'], $config['envs']);
+    $envSelected = io()->choice('Env to use', $envsName, current($envsName));
+
+    $envConfig = current(array_filter($config['envs'], fn(array $envData) => $envData['name'] === $envSelected));
+
+    $context = context()->withWorkingDirectory($config['src']);
+
+    if (fs()->exists($config['tmpStorage'])) {
+        io()->warning('Paused delivery found');
+
+        /** @var Release $release */
+        $release = unserialize(file_get_contents($config['tmpStorage']));
+        $sameEnvForOldRelease = $release->getEnv() === $envConfig['name'];
+
+        if ($sameEnvForOldRelease && YES === io()->choice('Resume paused delivery ?', [YES, NO], YES)) {
+            exit(resume($release, $context, $config, $envConfig));
+        }
+
+
+        if (!$sameEnvForOldRelease) {
+            io()->warning('Old delivery for another env, delete it...');
+        }
+
+        fs()->remove($config['tmpStorage']);
+        io()->info('Removed last delivery');
+    }
+
+    $isCurrentGitStateClean = str_contains(capture('git status', context: $context), 'nothing to commit, working tree clean');
+
+    if (!$isCurrentGitStateClean) {
+        io()->error('current git state is not clean');
+
+        exit(1);
+    }
+
+    io()->title('Getting data from Github');
+
+    $rawPrs = json_decode(http_request('POST', 'https://api.github.com/graphql', [
+        'body' => sprintf('{
+                    "query": "query ($owner: String!, $repo: String!, $first: Int!, $after: String) { repository(owner: $owner, name: $repo) { pullRequests(first: $first, after: $after) { edges { node { id number title commits(first: 200) { edges { node { commit { oid message committedDate } } } } closingIssuesReferences(first: 1) { edges { node { id number title  labels(first: 30) { edges { node {  name } } } } } } } } pageInfo { endCursor hasNextPage } } } }",
+                    "variables": {
+                        "owner": "%s", 
+                        "repo": "%s",    
+                        "first": 100,        
+                        "after": null           
+                    }
+                  }', $config['repo']['owner'], $config['repo']['name']),
+        'headers' => [
+            'Authorization' => sprintf('bearer %s', $config['githubToken'])
+        ]
+    ])->getContent(), true);
+
+    $rawPrsWithLinkedIssue = array_filter($rawPrs['data']['repository']['pullRequests']['edges'], fn(array $pr) => isset($pr['node']['closingIssuesReferences']['edges'][0]['node']));
+    $prs = array_map(fn(array $pr) => Pr::buildFromRawData($pr), $rawPrsWithLinkedIssue);
+
+    /** @var array<Pr> $prsToDeliver */
+    $prsToDeliver = array_filter($prs, fn(Pr $pr) => $pr->hasClosingIssueWithLabel($envConfig['toDeliverLabel']) || $pr->hasClosingIssueWithLabel($envConfig['alreadyDeliveredLabel']));
+
+    if (empty($prsToDeliver)) {
+        io()->warning(sprintf('No PR found for env %s', $envConfig['name']));
+        exit(0);
+    }
+
+    io()->title('About to deliver theses PRs');
+    io()->table(['PR #ID', 'PR title', 'issue #ID', 'issue title', 'Number of commit'], array_map(fn(Pr $pr) => [$pr->getId(), $pr->getTitle(), $pr->getClosingIssueId(), $pr->getClosingIssueTitle(), $pr->getCommitsCount()], $prsToDeliver));
+
+    if (YES !== io()->choice(sprintf('Ready to deliver these PRs for env %s ?', $envConfig['name']), [YES, NO], YES)) {
+        io()->error('Delivery aborted by user');
+        exit(1);
+    }
+
+    $deliveryBranchName = io()->ask('Enter delivery branch name', sprintf('%s_%s', $envConfig['name'], (new DateTimeImmutable())->format('Y-m-d_H-i-s')));
+    $baseBranchName = io()->ask('Enter base branch name', $config['baseBranch']);
+
+    io()->info(sprintf('updating %s', $baseBranchName));
+    capture(sprintf('git fetch --all && git checkout %s && git pull --rebase', $baseBranchName), context: $context);
+    io()->success(sprintf('%s is up to date', $baseBranchName));
+
+    io()->info(sprintf('creating delivery branch %s from %s', $deliveryBranchName, $baseBranchName));
+    capture(sprintf('git checkout -b %s', $deliveryBranchName), context: $context);
+    io()->success(sprintf('%s is created', $deliveryBranchName));
+
+    doMerge($prsToDeliver, $context, $config, $envConfig, $deliveryBranchName);
+
+    exit(0);
+}
+
+function resume(Release $release, Context $context, array $config, array $envConfig): int
+{
+
+
+    $conflictingPr = $release->getConflictingPr();
+    $conflictingCommit = $release->getConflictingCommit();
+
+    io()->title('Resume delivery');
+    io()->info(sprintf('Delivery paused at PR #%s "%s", commit SHA(%s) "%s"', $conflictingPr->getId(), $conflictingPr->getTitle(), $conflictingCommit->getSha(), $conflictingCommit->getMessage()));
+
+    $currentStatus = capture('git status', context: $context);
+    $mustApplyConflictResolution = str_contains($currentStatus, 'Changes to be committed');
+    if ($mustApplyConflictResolution) {
+        io()->title('Applying conflict resolution before resuming delivery');
+        io()->info($currentStatus);
+
+        if (YES !== io()->choice('Commit theses changes ?', [YES, NO], YES)) {
+            io()->error('Conflict resolution aborted');
+            return 1;
+
+        }
+
+        run('git cherry-pick --continue', context: $context->toInteractive());
+    }
+
+    $isCurrentGitStateClean = str_contains(capture('git status', context: $context), 'nothing to commit, working tree clean');
+
+    if (!$isCurrentGitStateClean) {
+        io()->error('current git state is not clean');
+
+        exit(1);
+    }
+
+
+    doMerge($release->getPrs(), $context, $config, $envConfig, $release->getBranchName());
+
+    fs()->remove($config['tmpStorage']);
+    io()->info('Removed last delivery');
+
+    return 0;
+}
+
+/**
+ * @param array<Pr> $prsToDeliver
+ */
+function doMerge(array $prsToDeliver, Context $context, array $config, array $envConfig, string $branchName): void
+{
+    io()->progressStart(count($prsToDeliver));
+    foreach ($prsToDeliver as $currentPrToDeliver) {
+        if ($currentPrToDeliver->isHandled()) continue;
+
+        io()->info(sprintf('handling PR #%s : %s', $currentPrToDeliver->getId(), $currentPrToDeliver->getTitle()));
+        foreach ($currentPrToDeliver->getCommits() as $commit) {
+            if ($commit->isHandled()) continue;
+            io()->info(sprintf('handling commit %s : "%s"', $commit->getSha(), $commit->getMessage()));
+            $commit->markHandled();
+            $cherryPick = run(sprintf('git cherry-pick --allow-empty %s', $commit->getSha()), context: $context->withQuiet()->withAllowFailure());
+
+            if ($cherryPick->isSuccessful()) {
+                io()->success(sprintf('commit %s OK', $commit->getSha()));
+
+                continue;
+            }
+
+            if (str_contains($cherryPick->getErrorOutput(), '--allow-empty')) {
+                io()->warning(sprintf('commit %s is already on the delivery branch', $commit->getSha()));
+                capture('git cherry-pick --skip', context: $context);
+
+                continue;
+            }
+
+            if (str_contains($cherryPick->getOutput(), 'CONFLICT')) {
+                io()->warning(sprintf('commit %s is in conflict', $commit->getSha()));
+                io()->info(sprintf('current delivery state stored in %s', $config['tmpStorage']));
+
+                $commit->markConflicted();
+                fs()->dumpFile($config['tmpStorage'], serialize(new Release($prsToDeliver, $currentPrToDeliver->getId(), $commit->getSha(), $envConfig['name'], $branchName)));
+
+                io()->warning('delivery pause here. Resolve conflict and restart command to resume delivery');
+
+                exit(3);
+            }
+
+            io()->error(sprintf('commit %s FAIL', $commit->getSha()));
+
+            io()->warning($cherryPick->getOutput());
+            io()->warning($cherryPick->getErrorOutput());
+
+            exit(1);
+        }
+
+        $currentPrToDeliver->markHandled();
+        io()->success(sprintf('PR #%s : %s is merged', $currentPrToDeliver->getId(), $currentPrToDeliver->getTitle()));
+        io()->progressAdvance();
+    }
+
+    io()->progressFinish();
+
+
+    if (io()->choice(sprintf('push new branch %s ?', $branchName), [YES, NO], YES)) {
+        run(sprintf('git push --set-upstream origin %s', $branchName), context: $context);
+
+        io()->success('branch pushed');
+    }
+}
+
+
+#[AsContext(name: 'init', default: true)]
+function initContext(): Context
+{
+    return (new Context())->withEnvironment([CONFIG_PATH_ENV_VAR => $_ENV[CONFIG_PATH_ENV_VAR] ?? DEFAULT_CONFIG_PATH]);
+}
+
+final readonly class Issue
+{
+    public function __construct(
+        private int    $id,
+        private string $title,
+        private array  $labels,
+    )
+    {
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function hasLabel(string $label): bool
+    {
+        return in_array($label, $this->labels);
+    }
+
+    public static function buildFromRawData(array $data): self
+    {
+        $labelsData = !empty($data['labels']['edges']) ? $data['labels']['edges'] : [];
+
+        return new self(
+            $data['number'],
+            $data['title'],
+            array_map(fn(array $labelData) => $labelData['node']['name'], $labelsData)
+        );
+    }
+
+}
+
+final class Pr
+{
+    private bool $handled = false;
+
+    /**
+     * @param array<Commit> $commits
+     */
+    public function __construct(
+        private readonly int    $id,
+        private readonly string $title,
+        private readonly Issue  $closingIssue,
+        private readonly array  $commits
+    )
+    {
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function getClosingIssueId(): int
+    {
+        return $this->closingIssue->getId();
+    }
+
+    public function getClosingIssueTitle(): string
+    {
+        return $this->closingIssue->getTitle();
+    }
+
+    public function getClosingIssue(): Issue
+    {
+        return $this->closingIssue;
+    }
+
+    public function getCommits(): array
+    {
+        return $this->commits;
+    }
+
+    public function hasClosingIssueWithLabel(string $label): bool
+    {
+        return $this->closingIssue->hasLabel($label);
+    }
+
+    public function getCommitsCount(): int
+    {
+        return count($this->commits);
+    }
+
+    public function isHandled(): bool
+    {
+        return $this->handled;
+    }
+
+    public function markHandled(): void
+    {
+        $this->handled = true;
+    }
+
+
+    public static function buildFromRawData(array $data): self
+    {
+        $prData = $data['node'];
+        return new self(
+            $prData['number'],
+            $prData['title'],
+            Issue::buildFromRawData($prData['closingIssuesReferences']['edges'][0]['node']),
+            array_map(fn(array $commitData) => Commit::buildFromRawData($commitData), $prData['commits']['edges']),
+        );
+    }
+}
+
+final class Commit
+{
+    private bool $handled = false;
+    private bool $conflict = false;
+
+    public function __construct(
+        private readonly string            $sha,
+        private readonly string            $message,
+        private readonly DateTimeImmutable $date,
+    )
+    {
+    }
+
+    public function getSha(): string
+    {
+        return $this->sha;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    public function getDate(): DateTimeImmutable
+    {
+        return $this->date;
+    }
+
+    public function isHandled(): bool
+    {
+        return $this->handled;
+    }
+
+    public function markHandled(): void
+    {
+        $this->handled = true;
+    }
+
+    public function isConflict(): bool
+    {
+        return $this->conflict;
+    }
+
+    public function markConflicted(): void
+    {
+        $this->conflict = true;
+    }
+
+    public function markConflictFree(): void
+    {
+        $this->conflict = true;
+    }
+
+
+    public static function buildFromRawData(array $data): self
+    {
+        $commitData = $data['node']['commit'];
+
+        return new self(
+            $commitData['oid'],
+            $commitData['message'],
+            new DateTimeImmutable($commitData['committedDate']),
+        );
+    }
+
+}
+
+final class Release
+{
+
+    /**
+     * @param array<Pr> $prs
+     */
+    public function __construct(
+        private array  $prs,
+        private int    $currentPrId,
+        private string $currentCommitSha,
+        private string $env,
+        private string $branchName
+    )
+    {
+    }
+
+    public function getPrs(): array
+    {
+        return $this->prs;
+    }
+
+    public function getCurrentPrId(): int
+    {
+        return $this->currentPrId;
+    }
+
+    public function getCurrentCommitSha(): int
+    {
+        return $this->currentCommitSha;
+    }
+
+    public function getEnv(): string
+    {
+        return $this->env;
+    }
+
+    public function getBranchName(): string
+    {
+        return $this->branchName;
+    }
+
+
+
+    public function getConflictingPr(): Pr
+    {
+        return current(array_filter($this->prs, fn(Pr $pr) => $pr->getId() === $this->currentPrId));
+    }
+
+    public function getConflictingCommit(): Commit
+    {
+        return current(array_filter($this->getConflictingPr()->getCommits(), fn(Commit $commit) => $commit->getSha() === $this->currentCommitSha));
+    }
+}
