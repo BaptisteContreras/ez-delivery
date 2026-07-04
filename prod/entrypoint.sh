f3a0215232e9527b86287fadedfe40a256ce5f63
Baptiste CONTRERAS <38988658+BaptisteContreras@users.noreply.github.com>
2026-07-04 11:03:30 +0200

    build: optimize prod image and reduce size

--- prod/entrypoint.sh
+++ prod/entrypoint.sh
@@ -1,5 +1,9 @@
-#!/bin/bash
+#!/bin/sh
+
+if [ -z "$USER" ]; then
+    echo "USER environment variable must be set to the host UID (e.g. -e USER=\$(id -u))" >&2
+    exit 1
+fi
 
 usermod -u "$USER" ez-delivery
-
-runuser -u ez-delivery -- "${@:1}"
+exec su-exec ez-delivery "$@"
